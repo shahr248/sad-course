e568cb14acf3fe9ef447120efe42d9fe24bf4d2f
shahr248 <shacharbe4@gmail.com>
2026-06-21 12:05:42 +0300

    Improve EmploymentDepartmentPanel UI: better colors, fonts, button sizing, and Back button visibility

--- PEDIS/EmploymentDepartmentPanel.Designer.cs
+++ PEDIS/EmploymentDepartmentPanel.Designer.cs
@@ -32,80 +32,99 @@ namespace PEDIS
             this.SuspendLayout();
 
             // lblTitle
-            this.lblTitle.AutoSize = true;
-            this.lblTitle.Font = new System.Drawing.Font("Arial", 14F, System.Drawing.FontStyle.Bold);
-            this.lblTitle.Location = new System.Drawing.Point(20, 20);
+            this.lblTitle.AutoSize = false;
+            this.lblTitle.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold);
+            this.lblTitle.Location = new System.Drawing.Point(20, 15);
             this.lblTitle.Name = "lblTitle";
-            this.lblTitle.Size = new System.Drawing.Size(200, 22);
+            this.lblTitle.Size = new System.Drawing.Size(500, 35);
             this.lblTitle.TabIndex = 0;
             this.lblTitle.Text = "Employment Departments";
+            this.lblTitle.ForeColor = System.Drawing.Color.FromArgb(0, 51, 102);
 
             // lvDepartments
             this.lvDepartments.FullRowSelect = true;
-            this.lvDepartments.Location = new System.Drawing.Point(20, 50);
+            this.lvDepartments.GridLines = true;
+            this.lvDepartments.Location = new System.Drawing.Point(20, 60);
             this.lvDepartments.Name = "lvDepartments";
-            this.lvDepartments.Size = new System.Drawing.Size(900, 300);
+            this.lvDepartments.Size = new System.Drawing.Size(984, 350);
             this.lvDepartments.TabIndex = 1;
             this.lvDepartments.UseCompatibleStateImageBehavior = false;
             this.lvDepartments.View = System.Windows.Forms.View.Details;
+            this.lvDepartments.BackColor = System.Drawing.Color.White;
+            this.lvDepartments.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
 
             // Add columns
             this.lvDepartments.Columns.Add("ID", 50);
-            this.lvDepartments.Columns.Add("Code", 100);
-            this.lvDepartments.Columns.Add("Name", 150);
-            this.lvDepartments.Columns.Add("Location", 150);
+            this.lvDepartments.Columns.Add("Code", 120);
+            this.lvDepartments.Columns.Add("Name", 200);
+            this.lvDepartments.Columns.Add("Location", 200);
             this.lvDepartments.Columns.Add("Capacity", 100);
-            this.lvDepartments.Columns.Add("Active", 70);
+            this.lvDepartments.Columns.Add("Active", 80);
 
             // btnView
-            this.btnView.Location = new System.Drawing.Point(20, 360);
+            this.btnView.Location = new System.Drawing.Point(20, 420);
             this.btnView.Name = "btnView";
-            this.btnView.Size = new System.Drawing.Size(75, 23);
+            this.btnView.Size = new System.Drawing.Size(100, 35);
             this.btnView.TabIndex = 2;
             this.btnView.Text = "View";
-            this.btnView.UseVisualStyleBackColor = true;
+            this.btnView.BackColor = System.Drawing.Color.FromArgb(0, 102, 204);
+            this.btnView.ForeColor = System.Drawing.Color.White;
+            this.btnView.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
+            this.btnView.UseVisualStyleBackColor = false;
             this.btnView.Click += new System.EventHandler(this.btnView_Click);
 
             // btnAdd
-            this.btnAdd.Location = new System.Drawing.Point(105, 360);
+            this.btnAdd.Location = new System.Drawing.Point(130, 420);
             this.btnAdd.Name = "btnAdd";
-            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.Size = new System.Drawing.Size(100, 35);
             this.btnAdd.TabIndex = 3;
             this.btnAdd.Text = "Add";
-            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.BackColor = System.Drawing.Color.FromArgb(46, 204, 113);
+            this.btnAdd.ForeColor = System.Drawing.Color.White;
+            this.btnAdd.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
+            this.btnAdd.UseVisualStyleBackColor = false;
             this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
 
             // btnEdit
-            this.btnEdit.Location = new System.Drawing.Point(190, 360);
+            this.btnEdit.Location = new System.Drawing.Point(240, 420);
             this.btnEdit.Name = "btnEdit";
-            this.btnEdit.Size = new System.Drawing.Size(75, 23);
+            this.btnEdit.Size = new System.Drawing.Size(100, 35);
             this.btnEdit.TabIndex = 4;
             this.btnEdit.Text = "Edit";
-            this.btnEdit.UseVisualStyleBackColor = true;
+            this.btnEdit.BackColor = System.Drawing.Color.FromArgb(241, 196, 15);
+            this.btnEdit.ForeColor = System.Drawing.Color.White;
+            this.btnEdit.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
+            this.btnEdit.UseVisualStyleBackColor = false;
             this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
 
             // btnDelete
-            this.btnDelete.Location = new System.Drawing.Point(275, 360);
+            this.btnDelete.Location = new System.Drawing.Point(350, 420);
             this.btnDelete.Name = "btnDelete";
-            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.Size = new System.Drawing.Size(100, 35);
             this.btnDelete.TabIndex = 5;
             this.btnDelete.Text = "Delete";
-            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.BackColor = System.Drawing.Color.FromArgb(231, 76, 60);
+            this.btnDelete.ForeColor = System.Drawing.Color.White;
+            this.btnDelete.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
+            this.btnDelete.UseVisualStyleBackColor = false;
             this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
 
             // btnBack
-            this.btnBack.Location = new System.Drawing.Point(20, 700);
+            this.btnBack.Location = new System.Drawing.Point(20, 470);
             this.btnBack.Name = "btnBack";
-            this.btnBack.Size = new System.Drawing.Size(75, 23);
+            this.btnBack.Size = new System.Drawing.Size(100, 35);
             this.btnBack.TabIndex = 6;
-            this.btnBack.Text = "Back";
-            this.btnBack.UseVisualStyleBackColor = true;
+            this.btnBack.Text = "← Back";
+            this.btnBack.BackColor = System.Drawing.Color.FromArgb(149, 165, 166);
+            this.btnBack.ForeColor = System.Drawing.Color.White;
+            this.btnBack.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
+            this.btnBack.UseVisualStyleBackColor = false;
             this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
 
             // EmploymentDepartmentPanel
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.Window;
+            this.BackColor = System.Drawing.Color.FromArgb(236, 240, 241);
             this.Controls.Add(this.btnBack);
             this.Controls.Add(this.btnDelete);
             this.Controls.Add(this.btnEdit);
